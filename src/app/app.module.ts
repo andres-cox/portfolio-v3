060f094a56152ccc903519719b03080227a9f8a3
andres-cox-mojix-com <andreshcox@gmail.com>
2020-07-13 18:52:25 -0300

    creating project details page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SectionsModule } from './sections/sections.module';
+import { ProjectDetailsComponent } from './pages/project-details/project-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProjectDetailsComponent,
   ],
   imports: [
     BrowserModule,
